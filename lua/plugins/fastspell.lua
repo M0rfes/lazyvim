80395c1350576b82df69e1dac7e7689390589ffa
M0rfes <fahimkhan20148@gmail.com>
2025-11-01 19:40:55 +0400

    init

---
+++ lua/plugins/fastspell.lua
@@ -0,0 +1,8 @@
+-- ~/.config/nvim/lua/plugins/fastspell.lua
+return {
+  "LucaSartore/fastspell.nvim", -- Replace with actual Fastspell repo path
+  config = function()
+    -- Put Fastspell configuration here if needed
+  end,
+  lazy = false,
+}
